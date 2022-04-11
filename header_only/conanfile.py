22cf22dcd2055f642979cdeae63fa3f75318706d
Rock Okechukwu <rockok10@gmail.com>
2022-04-11 15:30:20 +0100

    Initialize master-archive

---
+++ header_only/conanfile.py
@@ -0,0 +1,27 @@
+from conans import ConanFile, tools
+import os
+
+
+class LibnameConan(ConanFile):
+    name = "libname"
+    description = "Keep it short"
+    topics = ("libname", "logging")
+    url = "https://github.com/bincrafters/conan-libname"
+    homepage = "https://github.com/original_author/original_lib"
+    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    no_copy_source = True
+
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*", dst="include", src=include_folder)
+
+    def package_id(self):
+        self.info.header_only()
